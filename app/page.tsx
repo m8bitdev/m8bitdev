b347936120e2b738cbad522052162ca9a8a1cd0e
revelsprk <revelsprk@gmail.com>
2026-05-14 18:29:59 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,28 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div>
+      {/* Header */}
+      <div className="bg-white px-4 py-2 flex items-center border-b border-slate-200">
+        <h1 className="text-xl font-semibold">m8bit</h1>
+        <div className="flex items-center ml-auto space-x-3">
+          <p className="px-3 py-1 bg-slate-100 rounded-full">About Me</p>
+          <p className="px-3 py-1 bg-slate-100 rounded-full">Products</p>
+          <p className="px-3 py-1 bg-slate-100 rounded-full">Articles</p>
+          <p className="px-3 py-1 bg-slate-900 text-white font-semibold rounded-md">Contact</p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      </div>
+      {/* Main */}
+      <div className="md:container mx-auto my-8">
+        <h2 className="text-4xl font-semibold">About Me</h2>
+        <div className="mt-4 flex">
+          <Image src="/icon.png" alt="Icon" width={100} height={100} className="w-48 rounded-full border-2 border-slate-200" />
+          <div className="ml-3">
+            <h3 className="text-2xl font-semibold">m8bit</h3>
+            <p className="text-sm text-slate-400">Front-end Engineer / Designer</p>
+          </div>
         </div>
-      </main>
+      </div>
     </div>
-  );
+  )
 }
